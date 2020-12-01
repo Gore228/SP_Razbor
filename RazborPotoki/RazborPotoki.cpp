e70114ac35a5b57afb728ed51175aca10d417d7f
Егор Соловьев <d-rom93@mail.ru>
2020-12-01 10:45:07 +0300

    Добавьте файлы проекта.

---
+++ RazborPotoki/RazborPotoki.cpp
@@ -0,0 +1,92 @@
+﻿#include<Windows.h>
+#include<stdlib.h>
+#include<process.h>
+#include<tchar.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+TCHAR WinName[] = _T("MainFrame");
+HWND hwnd;
+int cxClient, cyClient;
+int APIENTRY _tWinMain(HINSTANCE This,
+	HINSTANCE Prev,
+	LPTSTR cmd,
+	int mode)
+{
+	MSG msg;
+	WNDCLASS wc;
+	wc.hInstance = This;
+	wc.lpszClassName = WinName;
+	wc.lpfnWndProc = WndProc;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.lpszMenuName = NULL;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = CreateSolidBrush(RGB(0, 0, 0));
+	if (!RegisterClass(&wc)) return 0;
+	hwnd = CreateWindow(WinName,
+		_T("RandomRectangles"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		HWND_DESKTOP,
+		NULL,
+		This,
+		NULL);
+	ShowWindow(hwnd, mode);
+	UpdateWindow(hwnd);
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return 0;
+
+}
+
+VOID Thread(PVOID pvoid)
+{
+	HBRUSH hbrush;
+	HDC hdc;
+	int xLeft, xRight, yTop, yBottom, iRed, iGreen, iBlue;
+	while (TRUE)
+	{
+		if (cxClient != 0 || cyClient != 0)
+		{
+			xLeft = rand() % cxClient;
+			xRight = rand() % cxClient;
+			yTop = rand() % cyClient;
+			yBottom = rand() % cyClient;
+			iRed = rand() & 255;
+			iGreen = rand() & 255;
+			iBlue = rand() & 255;
+			hdc = GetDC(hwnd);
+			hbrush = CreateSolidBrush(RGB(iRed, iGreen, iBlue));
+			SelectObject(hdc, hbrush);
+			Rectangle(hdc, min(xLeft, xRight), min(yTop, yBottom), max(xLeft, xRight), max(yTop, yBottom));
+			ReleaseDC(hwnd, hdc);
+			DeleteObject(hbrush);
+		}
+	}
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CREATE:
+		_beginthread(Thread, 0, NULL);
+		return 0;
+	case WM_SIZE:
+		cxClient = LOWORD(lParam);
+		cyClient = HIWORD(lParam);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
